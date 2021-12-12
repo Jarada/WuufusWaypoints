47708c6c6f9513030e8e49665137878b9e50bcb1
Jarada <jarada0077@mac.com>
2021-12-12 11:02:47 +0000

    Recode WarpEffect
    We use overrides of the Enum instead of a switch statement.

--- src/main/java/com/github/jarada/waypoints/data/WarpEffect.java
+++ src/main/java/com/github/jarada/waypoints/data/WarpEffect.java
@@ -1,6 +1,5 @@
 package com.github.jarada.waypoints.data;
 
-import com.github.jarada.waypoints.PluginMain;
 import com.github.jarada.waypoints.Util;
 import org.bukkit.*;
 
@@ -8,82 +7,104 @@ import java.util.Arrays;
 
 public enum WarpEffect {
 
-    BLAZE,
-    BUBBLE,
-    ENDER,
-    NETHER,
-    QUIET,
-    THUNDER;
-
-    public void playLoadingEffectAtLocation(Location location) {
-        if (location == null)
-            return;
-        
-        if (this == ENDER || this == NETHER || this == THUNDER)
-            Util.playSound(location, Sound.BLOCK_PORTAL_TRIGGER);
-    }
-
-    public void playTickEffectAtLocation(Location location, int tick, boolean to) {
-        if (location == null)
-            return;
-
-        if (!to) {
-            if (this == BLAZE) {
+    BLAZE {
+        @Override
+        public void playTickEffectAtLocation(Location location, int tick, boolean to) {
+            if (location == null) return;
+            if (!to) {
                 Util.playSound(location, Sound.ENTITY_BLAZE_BURN);
                 Util.playEffect(location, Effect.MOBSPAWNER_FLAMES);
                 if (Arrays.stream(Particle.values()).anyMatch(t -> t.name().equals("CAMPFIRE_SIGNAL_SMOKE"))) {
                     Util.playParticle(location, Particle.CAMPFIRE_SIGNAL_SMOKE, 50);
                 }
-                return;
             }
+            super.playTickEffectAtLocation(location, tick, to);
+        }
 
-            if (this == BUBBLE) {
+        @Override
+        public void playWarpingEffectAtLocation(Location location, boolean to) {
+            if (location == null) return;
+            Util.playSound(location, Sound.ENTITY_BLAZE_AMBIENT);
+        }
+    },
+    BUBBLE {
+        @Override
+        public void playTickEffectAtLocation(Location location, int tick, boolean to) {
+            if (location == null) return;
+            if (!to) {
                 Util.playSound(location, Sound.BLOCK_BUBBLE_COLUMN_UPWARDS_AMBIENT);
                 Util.playParticle(location, Particle.NAUTILUS, 50);
-                return;
+            } else if (tick == 4) {
+                Util.playParticle(location, Particle.NAUTILUS, 50);
             }
+            super.playTickEffectAtLocation(location, tick, to);
+        }
 
-            if (this == NETHER) {
+        @Override
+        public void playWarpingEffectAtLocation(Location location, boolean to) {
+            if (location == null) return;
+            Util.playSound(location, Sound.BLOCK_BUBBLE_COLUMN_BUBBLE_POP);
+        }
+    },
+    ENDER {
+        @Override
+        public void playLoadingEffectAtLocation(Location location) {
+            if (location == null) return;
+            Util.playSound(location, Sound.BLOCK_PORTAL_TRIGGER);
+        }
+
+        @Override
+        public void playWarpingEffectAtLocation(Location location, boolean to) {
+            if (location == null) return;
+            Util.playSound(location, Sound.ENTITY_ENDERMAN_TELEPORT);
+        }
+    },
+    NETHER {
+        @Override
+        public void playLoadingEffectAtLocation(Location location) {
+            if (location == null) return;
+            Util.playSound(location, Sound.BLOCK_PORTAL_TRIGGER);
+        }
+
+        @Override
+        public void playTickEffectAtLocation(Location location, int tick, boolean to) {
+            if (location == null) return;
+            if (!to || tick == 4) {
                 Util.playParticle(location, Particle.PORTAL, 50);
-                return;
             }
+            super.playTickEffectAtLocation(location, tick, to);
+        }
 
+        @Override
+        public void playWarpingEffectAtLocation(Location location, boolean to) {
+            if (location == null) return;
+            Util.playSound(location, Sound.ENTITY_GHAST_SHOOT);
+        }
+    },
+    QUIET,
+    THUNDER {
+        @Override
+        public void playLoadingEffectAtLocation(Location location) {
+            if (location == null) return;
+            Util.playSound(location, Sound.BLOCK_PORTAL_TRIGGER);
+        }
+
+        @Override
+        public void playWarpingEffectAtLocation(Location location, boolean to) {
+            if (location == null) return;
+            location.getWorld().strikeLightningEffect(location);
+        }
+    };
+
+    public void playLoadingEffectAtLocation(Location location) {}
+
+    public void playTickEffectAtLocation(Location location, int tick, boolean to) {
+        if (location == null) return;
+        if (!to) {
             Util.playEffect(location, Effect.ENDER_SIGNAL);
-        } else {
-            if (tick == 4) {
-                if (this == BUBBLE) {
-                    Util.playParticle(location, Particle.NAUTILUS, 50);
-                }
-                if (this == NETHER) {
-                    Util.playParticle(location, Particle.PORTAL, 50);
-                }
-            }
         }
     }
 
-    public void playWarpingEffectAtLocation(Location location, boolean to) {
-        if (location == null)
-            return;
-        
-        switch (this) {
-            case BLAZE:
-                Util.playSound(location, Sound.ENTITY_BLAZE_AMBIENT);
-                break;
-            case BUBBLE:
-                Util.playSound(location, Sound.BLOCK_BUBBLE_COLUMN_BUBBLE_POP);
-                break;
-            case ENDER:
-                Util.playSound(location, Sound.ENTITY_ENDERMAN_TELEPORT);
-                break;
-            case NETHER:
-                Util.playSound(location, Sound.ENTITY_GHAST_SHOOT);
-                break;
-            case THUNDER:
-                location.getWorld().strikeLightningEffect(location);
-                break;
-            default:
-                break;
-        }
-    }
+    public void playWarpingEffectAtLocation(Location location, boolean to) {}
 
 }
